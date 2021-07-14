034c47079b30bc60ba9ddd5d12c6ad9f78d28138
anjanettecastillo15 <adventureswithanjanette@gmail.com>
2021-07-14 19:51:17 +0800

    changed the logo at the navbar with transparent bg

--- login.php
+++ login.php
@@ -62,7 +62,7 @@ if($_SERVER['REQUEST_METHOD'] == "POST")
     <!--NAVBAR START-->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top mb-5">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#"><img src="PNR_logo.png" alt="PNR logo"></a>
+        <a class="navbar-brand" href="#"><img src="pnrots.png" alt="PNR logo"></a>
             <span class="navbar-text">PNR Online Ticketing System</span>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
